a823682e76a5656bee1240bc5b679c32ec112ee8
balkov13 <kovacs.balint1529@gmail.com>
2023-04-26 20:44:28 +0200

    implemented HCP with asynchronous updating

--- simulation/hco_simulation.py
+++ simulation/hco_simulation.py
@@ -8,34 +8,34 @@ class HomogeneousContactProcessSimulationStrategy(SimulationStrategy):
 
     def __init__(self):
         super().__init__()
+        self.neighbors = np.empty_like(self.occupied_cells, dtype=tuple)
+        n = self.grid_size
+        for i in range(self.grid_size):
+            for j in range(self.grid_size):
+                self.neighbors[i][j] = (
+                    ((i+1)%n, j),
+                    (i, (j+1)%n),
+                    ((i-1)%n, j),
+                    (i, (j-1)%n)
+                )
 
     def simulatePopularization(self):
-        
         self.changes.clear()
-        mask = np.pad(self.occupied_cells, ((1, 1), (1, 1)), mode='constant')
-        neighbors = (
-            mask[:-2, 1:-1]
-            + mask[2:, 1:-1]
-            + mask[1:-1, :-2]
-            + mask[1:-1, 2:]
-        )
-        neighbors = neighbors > 0
-        random_numbers = np.random.rand(*self.occupied_cells.shape)
-        become_occupied = (random_numbers < self.c) & (~self.occupied_cells) & neighbors
-        become_unoccupied = (random_numbers < self.e) & self.occupied_cells
-        self.occupied_cells[become_occupied] = True
-        self.occupied_cells[become_unoccupied] = False
+        for idx in range(self.grid_size**2):
+            #Sample a random cell
+            rand_cell = (np.random.randint(self.grid_size), np.random.randint(self.grid_size))
+            random_number = np.random.rand()
 
-        changed_indices = np.argwhere(become_occupied | become_unoccupied)
-        changed_indices_tuples = [tuple(x) for x in changed_indices]
-
-        self.changes.update(changed_indices_tuples)
-        n_changes = len(self.changes)
-        if not n_changes:
-            return
-        for i in range(n_changes):
-            pos = tuple(next(iter(self.changes)))
-            if self.occupied_cells[pos]:
-                self.occupied_cells[pos] = False
-            else:
-                self.occupied_cells[pos] = True
+            if not self.occupied_cells[rand_cell]:
+                #Count the number of occupied neighbors of the random cell
+                loc_neigh = self.neighbors[rand_cell]
+                k = 0
+                for neigh in loc_neigh:
+                    k += self.occupied_cells[neigh]
+                
+                if random_number < self.c * k/4:
+                    self.occupied_cells[rand_cell] = 1
+                    self.changes.add(rand_cell)
+            elif random_number < self.e:
+                self.occupied_cells[rand_cell] = 0
+                self.changes.add(rand_cell)
